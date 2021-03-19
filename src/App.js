488730a0e3b0584f0a7962f3d952ba7b33419bc2
Andrew McCarthy <amccarthy3950@gmail.com>
2021-03-19 17:23:48 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import axios from'axios'
+import axios from 'axios'
 import Character from './components/Character'
-
-
+import styled from 'styled-components'
+import { Container } from 'reactstrap';
 
 
 const App = () => {
-  const[character, setCharacter] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const Url = `https://swapi.dev/api/people/`
 
-  useEffect(() =>{
-    axios
-    .get(`https://rickandmortyapi.com/api/character`)
-    .then(res =>{
-      console.log(res.data.results)
-      setCharacter(res.data.results)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  },[]);
+  const [currentPeople, setCurrentPeople] = useState([]);
+
+  useEffect (()=> {
+    const fetchData = ()=> {
+      axios.get(` ${Url}`)
+      .then(res => {
+        setCurrentPeople(res.data)
+      })
+      .catch(err => {
+        debugger
+        console.log(err)
+      })
+    }
+    fetchData()
+  },[])
+
+  const People = props => {
+    const {data} = props
+
+    const StyledDiv = styled.div`
+    width: 40%;
+    text-align:center;
+    `
+
+    return(
+    <StyledDiv>
+      <h2>{props.data.name}</h2>
+      <Character data={data} />
+    </StyledDiv>)
+  }
+  
+  const StyledSpan = styled.span`
+   font-family: 'Press Start 2P', cursive;
+   color: yellow;
+   font-weight: bold;
+   font-size:3rem;
+  `;
+
+  const Container =  styled.div`
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items: center;
+  align-content: center;
+  `;
 
   return (
-
-     <div className="App">
-      <h1 className="Header">Characters</h1>
-      {character.map(item =>
-         <Character eachCharacter={item} key={item.id}/>
-        )}
-
-     </div>
-
+    <div className="App">
+      <h1 className="Header">Characters of <StyledSpan>Star Wars</StyledSpan>!</h1>
+        <Container>
+        {
+          currentPeople.map(person => {
+            return <People key={person.id} data={person} />
+          })
+        }
+        </Container>
+    </div>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,40 @@
 // Write your Character component here
-import React from 'react'
+import React, { useState } from 'react'
+import { Collapse, Button, CardBody, Card } from 'reactstrap'
+import styled from 'styled-components'
+import axios from 'axios'
 
 
+const Character = props => {
 
-export default function createCharacter({eachCharacter}){
-    return (
-        <div>
-    <h1 className = 'Name'>{eachCharacter.name}</h1>    
-    <img src = {eachCharacter.image} alt=''/>
-    <p className = 'Status'>{eachCharacter.status}</p>
-    <p className = 'Species'>{eachCharacter.species}</p>
-        </div>
+    const {data} = props
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+  
+    const StyledText = styled.div`
+    color: #0c0c0b;
+    text-align:justify;
+    background: #e5e4e2;
+    margin: 0 auto;
+    padding: 2rem;
+    `
 
 
-    )
-
+    return(
+    <div>
+        <Button color="secondary" onClick={toggle} style={{ marginBottom: '1rem' }}>More Info</Button>
+        <Collapse isOpen={isOpen}>
+        <Card style={{ width: '18rem' }} style={{display: 'flex', flexDirection: 'row'}}>
+            <CardBody style={{ width: '18rem' }}>
+                <StyledText>
+                    <p>`{data.name} has eyes that are {data.eye_color} and was born {data.birth_year}. `</p>
+                    <p>Lucas ipsum dolor sit amet vader thrawn han dantooine mace windu luuke antilles mace twi'lek. Padmé solo calamari sebulba. Lars jinn kenobi chewbacca. Solo organa c-3po binks darth solo kit. Moff kashyyyk skywalker chewbacca mon lobot jabba ewok. Qui-gon c-3po hoth yavin skywalker sith boba greedo antilles. Leia dagobah hutt wedge grievous. Lars darth r2-d2 lando calrissian thrawn tatooine wedge skywalker. Hutt leia moff kamino alderaan skywalker. Jango darth ben wedge. Skywalker hutt zabrak skywalker calrissian endor sidious darth moff.</p>
+                </StyledText>
+            </CardBody>
+        </Card>
+        </Collapse>
+    </div>
+    ) 
 }
+
+export default Character
